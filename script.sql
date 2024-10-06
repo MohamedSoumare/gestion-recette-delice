a4129ea4e4fb0bd95b864e9c880466366d039dda
Mohamed Soumare <mohamedsoumare17763@gmail.com>
2024-10-06 21:54:22 +0200

    update script.sql

--- script.sql
+++ script.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `gestion_recette_delice` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `gestion_recette_delice`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: gestion_recette_delice
@@ -51,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-06 21:49:21
+-- Dump completed on 2024-10-06 21:53:21
